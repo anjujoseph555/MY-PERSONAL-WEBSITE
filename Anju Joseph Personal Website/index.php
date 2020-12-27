73937ef9333349719aa99639dddbcb51a30c355c
anjujoseph555 <66248897+anjujoseph555@users.noreply.github.com>
2020-12-27 14:23:23 +0530

    Add files via upload

---
+++ Anju Joseph Personal Website/index.php
@@ -0,0 +1,174 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <link href="favicon.ico" rel="icon" type="image/x-icon" />
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+  <link rel="stylesheet" href="style.css">
+  <style>@import url('https://fonts.googleapis.com/css2?family=Courgette&display=swap');</style>
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <!-- Font Awesome -->
+  <script src="https://kit.fontawesome.com/b2b1ff1171.js" crossorigin="anonymous"></script>
+
+  <title>Welcome to Anju Joseph's Personal Website</title>
+</head>
+
+<body data-spy="scroll" data-target="#pb-navbar" data-offset="200">
+  <nav class="navbar navbar-expand-lg site-navbar navbar-light bg-light" id="pb-navbar">
+    <div class="container">
+      <button class="navbar-toggler" type="button" class="btn btn-light" data-toggle="collapse" data-target="#navbarsExample09" aria-controls="navbarsExample09" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse justify-content-md-center" id="navbarsExample09">
+        <ul class="navbar-nav">
+          <li class="nav-item"><a class="nav-link active" href="#section-home">HOME</a></li>
+          <li class="nav-item"><a class="nav-link" href="#section-about">ABOUT</a></li>
+          <li class="nav-item"><a class="nav-link" href="#section-skills">SKILLS</a></li>
+          <li class="nav-item"><a class="nav-link" href="#section-resume">RESUME</a></li>
+          <li class="nav-item"><a class="nav-link" href="#section-contact">CONTACT</a></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <section class="site-hero" style="background-image: url(Images/profilepic.jpg);" id="section-home" data-stellar-background-ratio="0.5">
+
+    <div class="container">
+      <div class="row intro-text align-items-center justify-content-center">
+        <div class="col-md-10 text-center pt-5 ">
+          <h1 class="site-heading site-animate fadeInUp site-animated heading">Hello, I'm <strong class="d-block">Anju Joseph</strong></h1>
+          <strong class="d-block text-white text-uppercase letter-spacing">Welcome to my Personal Website</strong>
+        </div>
+      </div>
+    </div>
+  </section>
+<!-- SEction about -->
+  <section class="site-section" id="section-about">
+  <div class="container">
+  <div class="row mb-5 align-items-center">
+  <div class="col-lg-7 pr-lg-5 mb-5 mb-lg-0">
+  <img src="Images/anjurajupic.jpg" alt="Image placeholder" class="img-fluid" height="500px" width="400px">
+  </div>
+  <div class="col-lg-5 pl-lg-5">
+  <div class="section-heading">
+  <h2><strong>About Me</strong></h2>
+  </div>
+  <p class="lead">Hello Everyone! I am Anju,currently pursuing my Bachelor's degree in Electronics and Communication from Govt.Model Engineering college.
+  <p class="lead">I am an optimisitic, curious and well-focussed developer.
+    Besides coding, I like singing and cooking. I am so glad to meet you,please do stay in touch.</p>
+  <p>
+  <a href="#section-contact" class="btn btn-primary px-4 py-2 btn-sm smoothscroll">Contact Me</a>
+  <a href="#section-resume" class="btn btn-primary px-4 py-2 btn-sm">Resume</a>
+  </p>
+  </div>
+  </div>
+  </div>
+  </section>
+  <!-- SKILLS -->
+  <section id="section-skills">
+  <div class="skills">
+    <h2 class="heading-skills"><strong>Skills</strong></h2>
+
+    <div class="row">
+
+      <div class="features col-lg-4">
+        <i class="icons fas fa-code fa-4x"></i>
+        <h4>Web Development</h4>
+        <p>
+          Being a web developer has helped me to extend my creativity and passion.
+          Knowledge in HTML, CSS, Bootstrap, JS, MERN Stack.
+        </p>
+      </div>
+      <div class="features col-lg-4">
+        <i class="icons fas fa-laptop-code fa-4x"></i>
+        <h4>Programming</h4>
+        <p>
+          I'm a perpetual learner with a passion for programming.
+          I love coding! I am proficient in C++/C and Python.
+        </p>
+      </div>
+
+      <div class="features col-lg-4">
+        <i class="icons fas fa-users fa-4x"></i>
+        <h4>Leadership Quality</h4>
+        <p>
+          Manifesting a can-do attitude, I embrace tough challenges with energy and enthusiasm.I have taken up several positions as a leader.
+            I'm also an active member of various organisations.
+        </p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="features col-lg-4">
+        <i class="icons fas fa-edit fa-4x"></i>
+        <h4>Content Development</h4>
+        <p>
+          I'm an amateur content developer .My articles have been published in various college social media pages.I find happiness in writing.
+        </p>
+      </div>
+
+      <div class="features col-lg-4">
+        <i class="icons fas fa-bookmark fa-4x"></i>
+        <h4>Proffesional Communcation</h4>
+        <p>
+          I'm an eloquent speaker with a gift of sociability.I have got full proficiency in English, Hindi and Malayalam.
+        </p>
+      </div>
+
+      <div class="features col-lg-4">
+        <i class="icons fas fa-running fa-4x"></i>
+        <h4>Sports</h4>
+        <p>
+          I'm a University level volleyball player and has particpated in KVS Regionals.I also love playing basketball and handball.
+          Sports has bestowed me teamspirit and tolerance.
+        </p>
+      </div>
+    </div>
+  </div>
+</section>
+<section id="section-resume">
+  <div class="container">
+    <br><br>
+    <div class="row mb-5">
+    <p class="resumecontent col-12 text-center">Recruiter? Have a look at my Resume</p>
+    <p class="col-12 text-center">
+     <a href="https://drive.google.com/file/d/1tfDeSQtI94U3ITJbMe-lJ0jOTIRjqC7D/view?usp=sharing" style="font-size:25px;" class="resumecontent btn btn-primary px-4 py-2 btn-sm">Resume</a>
+     <br><br>
+   </p>
+  </div>
+  </div>
+  </section>
+<section id="section-contact">
+  <footer class="site-footer">
+  <div class="container">
+  <div class="row mb-5">
+  <h2 class="footercontent col-12 text-center">Feel free to contact me!</h2>
+  <p class="col-12 text-center">
+  Copyright ©2020 Anju Joseph|Made with 💖
+  </p>
+  </div>
+  <div class="row mb-5">
+  <div class="col-md-12 text-center">
+  <p>
+  <a href="https://www.facebook.com/anju.joseph.77770/" class="social-item"><span><i class="footer-icon fab fa-facebook-f "></i></span></span></a>
+  <a href="https://www.instagram.com/_anjujoseph_/" class="social-item"><span><i class="footer-icon fab fa-instagram "></i></span></a>
+  <a href="https://mail.google.com/mail/?view=cm&fs=1&to=anjujoseph555@gmail.com" class="social-item"><span><i class="footer-icon fas fa-envelope"></i></span></a>
+  <a href="https://www.linkedin.com/in/anjujoseph555/" class="social-item"><span><i class="footer-icon fab fa-linkedin"></i></span></a>
+  <a href="https://github.com/anjujoseph555" class="social-item"><span><i class="footer-icon fab fa-github"></i></span></a>
+  </p>
+  </div>
+  </div>
+  </div>
+  </footer>
+</section>
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+</body>
+
+</html>
